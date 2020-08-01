44d617874e9e9473b7b6ea750a3f9fec8c31fe2b
Brijesh Kumar <brij10718@gmail.com>
2020-08-01 16:48:07 +0530

    Completed Boris Wilson typescript tutorial

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import "./index.css";
+// import {./app, ./person, ./player, ./result} from "./pluralsight";
 
 import "./chapter-01";
 
 import { add } from "./common/math";
 
-import "./pluralsight-01/app";
-// import "./pluralsight-01/person";
-// import "./pluralsight-01/player";
-// import "./pluralsight-01/result";
+import "./pluralsight/app";
+import "./pluralsight/person";
+import "./pluralsight/player";
+import "./pluralsight/result";
 
 export class C {
   private x = 17;
